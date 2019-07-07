11c3440ca706831d4ec2dd06e6a6817e04f7cb4a
seung-jae <lee@seung-jaeui-MacBookPro.local>
2019-07-07 23:57:24 +0900

    first commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'http://seung-jae.github.io/',
   pathPrefix: '/',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
